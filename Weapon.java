37348eb61caecc854eba94ba68805511ea2a4c8a
Fillian Janine Marcelino <44885917+janinemarce@users.noreply.github.com>
2019-02-18 09:45:22 +0800

    Update Weapon.java

--- Weapon.java
+++ Weapon.java
@@ -1,12 +1,4 @@
-
 package rpg;
-import java.util.Random;
-    public class Dice {
-        private Random r;
-        public Dice(){
-            r = new Random(); //Calls the imported Random util, just like how you import Scanner;
-        }
-        public int Roll(){
-            return r.nextInt(6)+1; //+1 is used so that Dice won't generate the number 0
-        }
-    }
+public interface Weapon {
+    public int useWeapon();
+}
