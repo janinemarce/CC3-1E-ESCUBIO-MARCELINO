1c416e683ec7898967d6bd0891578df7317c6619
Fillian Janine Marcelino <44885917+janinemarce@users.noreply.github.com>
2019-02-18 09:40:33 +0800

    Update Wizard.java

--- Wizard.java
+++ Wizard.java
@@ -1,31 +1,44 @@
 package rpg;
 public class Wizard extends Character {
-    static Dice dice = new Dice(); // call the dice
-    private final int maxmagic; // int maxmagic
-    private int currentmagic;  // int currentmagic
-    public Wizard (String n, int s, int d, int i){ //this is a constructor
-       super(n, s, d, i); // calling the Character class and its parameters (should be the same) because super is a keyword 
-        this.maxmagic = (dice.Roll()+9) + intelligence; // dice roll is less than or equal to 10, the maxmagic is modified by intelligence
+    static Dice dice = new Dice();
+    private final int maxmagic;
+    private int currentmagic;
+    private int strength;
+    public Wizard (String n, int s, int d, int i){
+       super( n, s, d, i); // calling the Character class and its parameters
+        this.maxmagic = (dice.Roll()+3) + intelligence; // dice roll is less than or equal to 10, the maxmagic is modified by intelligence
         this.currentmagic = maxmagic;
+        this.strength = s;
     }
-    public int castLightningBolt(){ //this is for lightningbolt
+    public int attack(){
+        return dice.Roll()+strength;
+    }
+    public int useArmor(){
+        return dice.Roll()+strength;
+    }
+    public int useWeapon(){
+        return dice.Roll()+strength;
+    }
+    public int castLightningBolt(){
         if (currentmagic >= 5){ // lightning bolt cannot be cast if the current magic is less than 5
-            currentmagic = currentmagic - 5 ; // this will minus mana because u used lightbolt thus u minus
-            return dice.Roll() + intelligence; //the lightning bolt attack is random but modified by intelligence
+            currentmagic = currentmagic - 5 ;
+            return (dice.Roll()+5) + intelligence; //the lightning bolt attack is random but modified by intelligence
         } else {
             return 0;
         }
     }
-    public int castHeal(){ //this is for heal
+    public int castHeal(){
         if (currentmagic >= 8){ // heal cannot be cast if current magic is less than 8
-                currentmagic = currentmagic - 8; // this will minus mana because u used heal thus u minus
-            super.heal(dice.Roll()+intelligence); // calling the heal method from Character class
+                currentmagic = currentmagic - 8;
+            super.heal(dice.Roll()); // calling the heal method from Character class
             return heal;
     } else {
           return 0;
         }
     }
-  
+    public int getMaxMagic(){
+        return maxmagic;
+    }
     public int getCurrentMagic(){
         if(currentmagic <= 0){
          return 0; //This is zero. [Players can't have negative magic
