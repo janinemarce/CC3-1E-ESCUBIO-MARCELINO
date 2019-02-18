93b0a42f8572e5232f5ffd2473782edb63cdb8af
Fillian Janine Marcelino <44885917+janinemarce@users.noreply.github.com>
2019-02-18 09:41:09 +0800

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,5 +1,5 @@
 package rpg;
-public class Character {
+public abstract class Character implements Armor,Weapon {
        static Dice dice = new Dice();
     private final String Name;
     private final int strength;
@@ -17,17 +17,17 @@ public class Character {
       this.maxLife = dice.Roll() * 8 ; //Player's MaxLife is dice.Roll times any value you want
       this.currentLife = maxLife;
     }
-     public int attack(){ //INSTUCTIONS:  Some of the implementation (such as the attack()method) can be done in many different ways and is open to your interpretation.
-     return dice.Roll()+strength; //Dice Value + Strength = Damage Dealt
-     } 
+     public abstract int  attack();//INSTUCTIONS:  Some of the implementation (such as the attack()method) can be done in many different ways and is open to your interpretation.
+     //return dice.Roll()+strength; //Dice Value + Strength = Damage Dealt
+     
      public void wound(int damage){ //Wound subtracts the damage dealt to Player's currentLife
      currentLife = currentLife - damage;
      if(currentLife <= 0){
          currentLife = 0; //This is zero. [Players can't have negative health] if Player's current life gets lower than zero, it won't return a negative value, instead, it will return a zero value.
             }
         }
-     public void heal(int heal){ // this is the heal
-         this.heal = heal; //gets the variable of heal
+     public void heal(int heal){
+         this.heal = heal + intelligence;
          currentLife = currentLife + heal;
      } 
 /* default values from the given Laboratory Exercise #4 */     
@@ -43,5 +43,4 @@ public class Character {
      {return maxLife;}
      public int getCurrentLife()
      {return currentLife;}
-
 }
